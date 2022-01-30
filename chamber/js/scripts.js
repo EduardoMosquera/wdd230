e7f61a979c474d440d513e1657a45f4c596bc477
EduardoMosquera <mos21008@byui.edu>
2022-01-30 00:59:33 -0500

    responsive changes for large and medium devices

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,44 +1,11 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
 const d = new Date();
-const monthName = months[d.getMonth()];
-const dayName = daynames[d.getDay()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${monthName} ${d.getDate()}, ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
-// ****************************
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
+let year = d.getFullYear();
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let day = weekday[d.getDay()];
+let monthname = month[d.getMonth()];
 
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  date2.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
+document.querySelector(".currentyear").textContent += `\u00A9 ${year} Daule Chamber of Commerce`;
  
 function toggleMenu(){
   document.getElementById("menu").classList.toggle("open");
