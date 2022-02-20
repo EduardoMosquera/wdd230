236541bda9006fe317d79d7dc939443aa28a0e8e
EduardoMosquera <mos21008@byui.edu>
2022-02-19 23:33:18 -0500

    added last day since last visit

---
+++ chamber/js/visit.js
@@ -0,0 +1,20 @@
+// initialize display elements
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let dateVisited = Number(window.localStorage.getItem("last-visit"));
+
+
+dateVisited = new Date();
+
+// increment the number of visits.
+todayDate = new Date - dateVisited;
+// store the new number of visits value
+localStorage.setItem("last-visit", dateVisited);
+
+// show todays date.
+lastVisit = Date.now()
+
+let days = Math.round(Date.now() - lastVisit);
+visitsDisplay.textContent = days;
