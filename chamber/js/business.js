81d7b9037b723524734dc5c813f41d2ecf3ff7da
EduardoMosquera <mos21008@byui.edu>
2022-03-03 18:56:49 -0500

    update

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -6,32 +6,35 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject['prophets'];
+    const business = jsonObject['business'];
    
-    prophets.forEach(displayProphets);
+    business.forEach(displayProphets);
   });
 
 
 function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
+    let businessName = document.createElement('p');
+    let address = document.createElement('p');
     let birthday = document.createElement('p');
     let birthPlace = document.createElement('p');
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-    birthday.textContent = `Date of Birth: ${prophet.birthdate}`;
-    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    businessName.textContent = prophet.name;
+    address.textContent = prophet.address;
+    birthday.textContent = prophet.phone;
+    birthPlace.textContent = prophet.website;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.address);
     portrait.setAttribute('loading', 'lazy');
   
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
+    // Add/append the section(card) with the businessName element
+    card.appendChild(businessName);
+    card.appendChild(address);
     card.appendChild(birthday);
     card.appendChild(birthPlace);
     card.appendChild(portrait);
